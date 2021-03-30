68dd413402d0593183e87603d18848d248e44746
Devipatel382 <70477645+Devipatel382@users.noreply.github.com>
2021-03-30 17:26:16 -0400

    Add files via upload

---
+++ barter-app-stage-9-master/barter-app-stage-9-master/config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAyN3lBtLaMYcqpQaXIIBtDiU6GqWRXgjA",
+  authDomain: "barteringapp.firebaseapp.com",
+  projectId: "barteringapp",
+  storageBucket: "barteringapp.appspot.com",
+  messagingSenderId: "273816431720",
+  appId: "1:273816431720:web:699ad6056bb1767938dfdd"
+};
+
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore();
